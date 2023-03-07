ccd0be29264279a634ed779deedd18dc97eb6030
Kirillbasenko <95930608+Kirillbasenko@users.noreply.github.com>
2023-03-07 10:54:53 +0200

    new firebase2

--- src/fireBase.js
+++ src/fireBase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from "firebase/firestore";
 
    const firebaseConfig = {
-   apiKey: "AIzaSyCQSl7QvMqcfbDRABJeikz3-ONJP6WCWHg",
-   authDomain: "pet-project-photo.firebaseapp.com",
-   projectId: "pet-project-photo",
-   storageBucket: "pet-project-photo.appspot.com",
-   messagingSenderId: "760309131827",
-   appId: "1:760309131827:web:9c6acb7c5f6a24d14c9146",
-   measurementId: "G-S2MPZKTEYQ"
+      apiKey: "AIzaSyCQSl7QvMqcfbDRABJeikz3-ONJP6WCWHg",
+      authDomain: "pet-project-photo.firebaseapp.com",
+      projectId: "pet-project-photo",
+      storageBucket: "pet-project-photo.appspot.com",
+      messagingSenderId: "760309131827",
+      appId: "1:760309131827:web:9c6acb7c5f6a24d14c9146",
+      measurementId: "G-S2MPZKTEYQ"
    };
    export const app = initializeApp(firebaseConfig);
    export const db = getFirestore(app);
