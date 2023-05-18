5a5aac891810580489dd1114f644f8e121be739b
Kirillbasenko <95930608+Kirillbasenko@users.noreply.github.com>
2023-05-18 10:36:22 +0300

    new firebase13

--- src/fireBase.js
+++ src/fireBase.js
@@ -2,15 +2,15 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-   const firebaseConfig = {
-      apiKey: "AIzaSyCQSl7QvMqcfbDRABJeikz3-ONJP6WCWHg",
-      authDomain: "pet-project-photo.firebaseapp.com",
-      projectId: "pet-project-photo",
-      storageBucket: "pet-project-photo.appspot.com",
-      messagingSenderId: "760309131827",
-      appId: "1:760309131827:web:9c6acb7c5f6a24d14c9146",
-      measurementId: "G-S2MPZKTEYQ"
-   };
-   export const app = initializeApp(firebaseConfig);
-   export const db = getFirestore(app);
-   export const auth = getAuth(app);
+const firebaseConfig = {
+   apiKey: "AIzaSyBTPjw5_uC_zo67Vtsn5K-npfI_B3Vkah4",
+   authDomain: "pet-project-79d1c.firebaseapp.com",
+   projectId: "pet-project-79d1c",
+   storageBucket: "pet-project-79d1c.appspot.com",
+   messagingSenderId: "586431060714",
+   appId: "1:586431060714:web:4b0086938e508da0f21593",
+   measurementId: "G-LE9RYTZDE1"
+};
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
